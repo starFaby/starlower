4bba31a26f6b568131a84637367ca92a2f049ecf
faby star <starFaby>
2022-07-12 08:41:38 -0500

    ultima actualizacion

--- app/controllers/controllerAvances.py
+++ app/controllers/controllerAvances.py
@@ -1,6 +1,20 @@
-from flask import request, render_template as render
+import engineio
+from flask import request, render_template as render, redirect, url_for
+from flask_login import current_user
+from sqlalchemy import select
+from app.database.database import *
 
 class ControllerAvances:
 
     def controllerAvances():
-       return render("client/avances.html")
+        
+        if current_user.is_authenticated:
+            iduser = current_user.iduser
+            avances = Usercaso.query.join(Caso.query).filter(Usercaso.userid == iduser).filter(Caso.id == Usercaso.casoid)
+            #avances = Usercaso.query.join(Caso.query.all())
+            cas = Caso.query
+            for item in avances:
+                print(item.avance)
+            return render("client/avances.html", avances = avances )
+        else:
+            return redirect(url_for("loginin.onGetLogin"))
--- app/routes/routerAvances.py
+++ app/routes/routerAvances.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 from app.controllers.controllerAvances import ControllerAvances
 avances= Blueprint('avances', __name__)
-avances.route('/avances', methods=['GET'])(ControllerAvances.controllerAvances)
+avances.route('/avances', methods=['GET','POST'])(ControllerAvances.controllerAvances)
