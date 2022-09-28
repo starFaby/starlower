e0857ed9acc90965049c16a2d1412048c7a31cdd
faby star <starFaby>
2022-09-27 21:30:35 -0500

    url bd

--- app/config/config.py
+++ app/config/config.py
@@ -4,11 +4,9 @@ class Config:
     #---------------------------------------
     #-------servicio local-------------
     #---------------------------------------
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/lawerstar'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/lawerstar'
     #---------------------------------------
     #-------servicio en la nube-------------
     #---------------------------------------
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://uez9l7gzjykxiueb:wKOaTSdauIOy9f5W9I7h@brjdc5b3vysmljkebacf-mysql.services.clever-cloud.com/brjdc5b3vysmljkebacf'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://uez9l7gzjykxiueb:wKOaTSdauIOy9f5W9I7h@brjdc5b3vysmljkebacf-mysql.services.clever-cloud.com/brjdc5b3vysmljkebacf'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-
